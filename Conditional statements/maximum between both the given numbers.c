c8cc79274ac46ae58fa0b2460b008f21a1565533
Thiru-glitch12 <sakthijothi275@gmail.com>
2026-02-05 15:20:30 +0530

    Create maximum between both the given numbers.c

---
+++ Conditional statements/maximum between both the given numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int num1, num2;
+    printf("Enter two numbers: ");
+    scanf("%d%d", &num1, &num2);
+    if(num1 > num2)
+    {
+        printf("First number is maximum.");
+    }
+    else
+    {
+        printf("Second number is maximum.");
+    }
+
+    return 0;
+}
