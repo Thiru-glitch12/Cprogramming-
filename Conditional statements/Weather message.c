f30b99010db869185e666798282d671bfdefd88a
Thiru-glitch12 <sakthijothi275@gmail.com>
2026-02-17 12:21:21 +0530

    Add weather message based on temperature input

---
+++ Conditional statements/Weather message.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    if (t <= 0){
+        printf("Freezing weather");
+    }
+    else if (t <= 10){
+        printf("Very Cold weather");
+    }
+    else if (t <= 20){
+        printf("Cold weather");
+    }
+    else if (t <= 30){
+        printf("Normal temperature");
+    }
+    else if (t <= 40){
+        printf("Hot");
+    }
+    else{
+        printf("Very Hot");
+    }
+
+    return 0;
+}
